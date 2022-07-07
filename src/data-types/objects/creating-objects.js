e90ad36c14b52823093d541a10e52b4b14575bd5
Enky <enkh6at@yahoo.com>
2022-07-07 20:24:53 +0300

    exercise from object

--- src/data-types/objects/creating-objects.js
+++ src/data-types/objects/creating-objects.js
@@ -1,7 +1,15 @@
 // TODO: write code in this section to pass the tests. You will need to add new code
 // as well as modify some of the existing code
-const person = null
-const computer = null
+const person = {name: 'Jane',
+                age: 32}
+console.log(person)
+
+//creating object with computer
+const computer = { form: 'laptop',
+specs: {memory: '16GB', storage: '1TB'}}
+console.log(computer)
+
+
 
 // Do not edit this exported object
 module.exports = {
