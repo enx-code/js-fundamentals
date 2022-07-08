b8792427738810a3b65ad31becc367df2546b5cd
Enky <enkh6at@yahoo.com>
2022-07-08 23:18:00 +0300

    multiple conditions done

--- src/conditional-flow/multiple-conditions.js
+++ src/conditional-flow/multiple-conditions.js
@@ -6,7 +6,11 @@ const NUM = 9
 // Use conditional statements to set the value of the answerOne variable
 // to be true if the NUM variable is more than or equal to the LOWER variable
 // AND is less than or equal to the UPPER variable
-let answerOne
+let answerOne = true
+if (NUM >= LOWER && NUM<= UPPER) {
+    console.log(answerOne)
+
+}
 
 
 
@@ -21,6 +25,13 @@ const STR = null
 // Run the test after setting STR to 'Hello', then 'Goodbye', then any other value you like
 // to verify your code is correct
 let answerTwo
+if (STR === "Hello" || STR === "Goodbye") {
+    answerTwo = true
+    console.log(answerTwo)
+} else {
+    answerTwo = false
+    console.log(answerTwo)
+}
 
 
 
@@ -42,6 +53,20 @@ const AGE = 0
 // 13-19  | Teenager
 // 20+    | Adult
 let answerThree
+if (AGE === 0) {
+    answerThree = "Baby"
+    console.log(answerThree)
+} else if (AGE >= 1 || AGE <= 4) {
+    answerThree = "Toddler"
+    console.log(answerThree)
+} else if (AGE >= 5 || AGE <= 12){
+    answerThree = "Child"
+    console.log(answerThree)    
+} else if (AGE >= 13 || AGE <= 19) {
+    answerThree = "Teenager"
+} else  {
+    answerThree = "Adult"
+}
 
 
 // Run the test after changing the AGE value to verify you've successfully
