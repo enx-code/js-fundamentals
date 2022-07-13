f637f8fdfa8a967658789591d84a66644e226846
Enky <enkh6at@yahoo.com>
2022-07-13 15:46:57 +0300

    function with multiple arguments

--- src/functions/creating-functions-multiple-args.js
+++ src/functions/creating-functions-multiple-args.js
@@ -9,6 +9,17 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function allNum (lowNum, upNum) {
+  const arr = []
+  for (let i = lowNum; i <= upNum; i++) {
+    arr.push(i)  
+  }
+  return arr
+}
+
+console.log(allNum(1, 3))
+console.log(allNum(10, 13))
+console.log(allNum(-1, 1))
 
 // define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,9 +32,26 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function convert(string, number) {
+
+  let text = string.toUpperCase()
+  for (let i = 0; i < number; i++) {
+      text = text + '!'
+      
+  }
+
+
+  return text
+
+}
+console.log(convert("disaster", 5))
+console.log(convert("hello", 10))
+
+
+
 
 // change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  a: allNum, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: convert // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
 }
